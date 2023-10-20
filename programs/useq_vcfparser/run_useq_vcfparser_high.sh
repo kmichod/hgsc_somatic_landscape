d74cfdbd899a447b5c155d0e2c3159b4a2380045
kmichod <88340134+kmichod@users.noreply.github.com>
2023-10-20 15:18:56 -0700

    Update run_useq_vcfparser_high.sh

--- programs/useq_vcfparser/run_useq_vcfparser_high.sh
+++ programs/useq_vcfparser/run_useq_vcfparser_high.sh
@@ -1,21 +1,14 @@
 #!/bin/bash
-#SBATCH--time=10:00:00
-#SBATCH--nodes=1
-#SBATCH--ntasks=1
-#SBATCH--account=doherty
-#SBATCH --partition=redwood
-#SBATCH--mail-type=FAIL,END
-#SBATCH--mail-user=katherine.lawson-michod@hci.utah.edu
 
 ## this file runs the VCF annotator and parser which will filter called somatic variants based on specific criteria
 
 ## read depth -d, minimum tumor AF -m, minimum alternative observations -w, and BKZ score -z set based on criteria for low, medium, and high confidence criteria
 
-java -jar /uufs/chpc.utah.edu/common/PE/hci-bioinformatics1/TNRunner/BioApps/USeq/Apps/AnnotatedVcfParser -v /uufs/chpc.utah.edu/common/HIPAA/u1325930/aaces/hgsc_landscape/data -s /uufs/chpc.utah.edu/common/HIPAA/u1325930/aaces/hgsc_landscape/data/germline_highconfidence -d 30 -m 0.15 -w 4 -z 3
+java -jar ~/USeq/Apps/AnnotatedVcfParser -v ~/hgsc_somatic_landscape/data -s ~/hgsc_somatic_landscape/data/somatic_highconfidence -d 30 -m 0.15 -w 4 -z 3
 
 ## after vcf parser remove any VCFs failing these parameters as well as the hyper-mutated sample 
 
-cd /uufs/chpc.utah.edu/common/HIPAA/u1325930/aaces/hgsc_landscape/data/germline_highconfidence
+cd ~/hgsc_somatic_landscape/data/somatic_highconfidence
 
 rm *Fail*
 
