5476aa61dbf0614832653f8649c18b4c17b8c6ff
kmichod <88340134+kmichod@users.noreply.github.com>
2023-10-20 15:16:43 -0700

    Update qual_evs_filter.sh

--- programs/qual_evs_filter/qual_evs_filter.sh
+++ programs/qual_evs_filter/qual_evs_filter.sh
@@ -1,13 +1,8 @@
 #!bin/bash
-#SBATCH--time=1:00:00
-#SBATCH--account=doherty
-#SBATCH --partition=redwood
-#SBATCH--mail-type=FAIL,END
-#SBATCH--mail-user=katherine.lawson-michod@hci.utah.edu
 
-cd /uufs/chpc.utah.edu/common/HIPAA/u1325930/aaces/hgsc_landscape/data/somatic/vcfs/filtered/hg38/moderateconfidence/temp/
+cd /hgsc_somatic_landscape/data/somatic/vcfs/filtered/hg38/highconfidence/temp/
 
 for x in $(ls *.vcf) 
 do 
-vcffilter -f "FILTER = PASS & QUAL > 15" $x > /uufs/chpc.utah.edu/common/HIPAA/u1325930/aaces/hgsc_landscape/data/somatic/vcfs/filtered/hg38/moderateconfidence/${x%%.vcf}.vcf 
+vcffilter -f "FILTER = PASS & QUAL > 15" $x > hgsc_somatic_landscape/data/somatic/vcfs/filtered/hg38/highconfidence/${x%%.vcf}.vcf 
 done
